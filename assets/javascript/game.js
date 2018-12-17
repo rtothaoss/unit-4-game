7362bd5305bed04a442fc99c8f8e610eda2a69dd
Ross carmack <rosscarmack@Rosss-MBP.fios-router.home>
2018-12-16 21:37:17 -0600

    added random functions

---
+++ assets/javascript/game.js
@@ -0,0 +1,15 @@
+
+
+
+
+    // function newGame() {
+    //     var targetNumber = "";
+        
+    //     var random = Math.floor(Math.random() * 120) + 19;
+    //     targetNumber = random;
+    //     $('#target-number').append(targetNumber);
+     
+     
+    //  }
+     
+    //  newGame();
